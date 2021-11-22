ea4c17a3c9a7b5b259286451660bb5554f120c60
Scala Steward <me@scala-steward.org>
2021-11-22 21:33:17 +0100

    Update zio-interop-cats to 3.2.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val doobie = "0.13.4"
     val liquibase = "4.6.1"
     val zio = "1.0.12"
-    val zioCats = "2.5.1.0"
+    val zioCats = "3.2.9.0"
     val zioConfig = "1.0.10"
   }
 
