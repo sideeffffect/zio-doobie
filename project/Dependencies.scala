c1179d0e7533c1c09fd8cbc4751bdbd4ef167a79
Scala Steward <me@scala-steward.org>
2022-03-21 08:42:41 +0100

    Update zio-config-magnolia, ... to 2.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val liquibase = "4.9.0"
     val zio = "1.0.13"
     val zioCats = "3.2.9.1"
-    val zioConfig = "2.0.3"
+    val zioConfig = "2.0.4"
   }
 
   val doobie = "org.tpolecat" %% "doobie-hikari" % Versions.doobie
