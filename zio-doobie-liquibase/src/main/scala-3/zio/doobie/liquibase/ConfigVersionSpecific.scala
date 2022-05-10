7920e79d02d5ef803452bfbeb0dfc483d3f86ff3
Ondra Pelech <ondra.pelech@gmail.com>
2022-05-10 02:39:27 +0200

    fix Scala 3

--- zio-doobie-liquibase/src/main/scala-3/zio/doobie/liquibase/ConfigVersionSpecific.scala
+++ zio-doobie-liquibase/src/main/scala-3/zio/doobie/liquibase/ConfigVersionSpecific.scala
@@ -5,8 +5,6 @@ import zio.config.magnolia.Descriptor
 import zio.doobie.liquibase.ZIODoobieLiquibase.Config
 
 trait ConfigVersionSpecific {
-  implicit lazy val hikariDescriptor: ConfigDescriptor[doobie.hikari.Config] =
-    Descriptor.derived[doobie.hikari.Config].desc
   implicit lazy val configDescriptor: ConfigDescriptor[Config] =
     Descriptor.derived[Config].desc
 }
