cb4486c2ef38f157132d992cb4b8a792bd92374a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-07-12 06:43:32 +0200

    Update liquibase-core to 4.13.0 (#159)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val doobie = "1.0.0-RC2"
-    val liquibase = "4.12.0"
+    val liquibase = "4.13.0"
     val zio = "1.0.15"
     val zioCats = "3.2.9.1"
     val zioConfig = "2.0.4"
