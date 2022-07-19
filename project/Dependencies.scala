6e74618f770dd47501c7a998bca4935f0af175a7
Scala Steward <scala_steward@virtuslab.com>
2022-07-19 18:07:35 +0000

    Update zio-interop-cats to 3.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val doobie = "1.0.0-RC2"
     val liquibase = "4.13.0"
     val zio = "1.0.16"
-    val zioCats = "3.2.9.1"
+    val zioCats = "3.3.0"
     val zioConfig = "2.0.4"
   }
 
