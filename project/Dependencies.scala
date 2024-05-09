201740e974afa5c3a1831186dc03aa924dd7f7e2
Scala Steward <scala_steward@virtuslab.com>
2024-05-09 19:59:42 +0000

    Update zio to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val doobie = "1.0.0-RC5"
     val liquibase = "4.27.0"
-    val zio = "2.0.22"
+    val zio = "2.1.0"
     val zioCats = "23.1.0.2"
     val zioConfig = "4.0.2"
   }
