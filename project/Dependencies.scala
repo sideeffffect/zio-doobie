4f245299355166d7ffa7ba702aebb589551e7e9c
Scala Steward <scala_steward@virtuslab.com>
2024-05-21 23:37:22 +0000

    Update liquibase-core to 4.28.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val doobie = "1.0.0-RC5"
-    val liquibase = "4.27.0"
+    val liquibase = "4.28.0"
     val zio = "2.0.22"
     val zioCats = "23.1.0.2"
     val zioConfig = "4.0.2"
